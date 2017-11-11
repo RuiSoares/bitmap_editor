070a90901fb2653f3f36c4414170ea2a06bee8be
RuiSoares <ruisoares@Ruis-MBP.lan>
2017-11-11 19:26:46 +0000

    create run test that takes input from examples and compares it to output also from examples

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -5,6 +5,18 @@ require_relative '../lib/errors'
 
 describe BitmapEditor do
   describe 'run' do
-    #TODO read from examples test, compare to examples results
+    path = './examples'
+    Dir.foreach(path) do |filename|
+      next if filename == '.' || filename == '..' || (filename =~ /\Aresult/) == 0
+
+      it "returns right output for '#{filename}'" do
+        input_file = File.open("#{path}/#{filename}", 'r')
+        expected = File.read("#{path}/result_#{filename}")
+
+        expect { BitmapEditor.new.run(input_file) }.to output(expected).to_stdout
+
+        input_file.close
+      end
+    end
   end
 end
